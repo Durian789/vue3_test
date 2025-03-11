c19f0ecde54bce785be49e7eb25eae9326798366
Durian789 <319480931@qq.com>
2025-03-11 23:01:54 +0800

    [Feature]watch事件监听

--- src/components/Persion.vue
+++ src/components/Persion.vue
@@ -1,89 +1,73 @@
 <template>
   <div class="persion">
-    <h1>情况一、监视 ref 定义的 基本数据 类型 </h1>
-    <h2>当前求和为： {{sum}}</h2>
-    <button @click="chnageSum">点我sum加一</button>
-
-
-    <!-- <h1>情况二、监视 ref 定义的 对象数据 类型 </h1>
-    <h2>姓名： {{person.name}}</h2>
-    <h2>年龄： {{person.age}}</h2>
-    <button @click="changeName">改变姓名</button>    
-    <button @click="changeAge">改变年龄</button>    
-    <button @click="changePerson">改变整个人</button> -->
-
-    <h1>情况三、监视 reactive 定义的 对象数据 类型 </h1>
-    <h2>姓名： {{person.name}}</h2>
-    <h2>年龄： {{person.age}}</h2>
-    <button @click="changeName">改变姓名</button>    
-    <button @click="changeAge">改变年龄</button>    
-    <button @click="changePerson">改变整个人</button>
+    <h2>姓名： {{ person.name }}</h2>
+    <h2>年龄： {{ person.age }}</h2>
+    <h2>车： {{ person.cars.c1.name }} 、 {{ person.cars.c2.name }}</h2>
+    <button @click="changeName">改变姓名</button>
+    <button @click="changeAge">改变年龄</button>
+    <button @click="changeC1">改变C1</button>
+    <button @click="changeC2">改变C2</button>
+    <button @click="changeCar">改变车</button>
   </div>
 </template>
 <script lang="ts" setup name="Persion">
-import { reactive, ref, watch } from 'vue'
+import { reactive, ref, watch } from "vue";
 
-// 数据
-let sum = ref(0)
-// 方法
-function chnageSum() {
-  sum.value++
-}
-
-const stopWatch = watch(sum, (newValue, oldValue) => {
-  console.log('sum值变化了', sum.value, newValue, oldValue)
-  if (sum.value > 10) {
-    stopWatch()
-  }
-})
-
-//数据
-// let person = ref({
-//   name: '张三',
-//   age: 18
-// })  
-
-// function changeName() {
-//   person.value.name += '~'
-// }
-// function changeAge() {
-//   person.value.age += 1
-// }
-// function changePerson() {
-//   person.value = {
-//     name: '李四',
-//     age: 20
-//   }
-// }
-
-// watch(person, (newValue, oldValue) => {
-//   console.log('person值变化了', newValue, oldValue);
-// },{
-//   deep: true,
-//   immediate: true
-// })
-
-
-//三、监视 reactive 定义的 对象数据 类型  默认开启深度监听（隐式创建了深度监听）  并没法关闭 新旧值都是同一个
 let person = reactive({
-  name: '张三',
-  age: 18
-})  
+  name: "张三",
+  age: 18,
+  cars: {
+    c1: {
+      name: "保时捷1",
+      price: 100000,
+    },
+    c2: {
+      name: "保时捷2",
+      price: 100000,
+    },
+  },
+});
 
 function changeName() {
-  person.name += '~'
+  person.name += "~";
 }
 function changeAge() {
-  person.age += 1
+  person.age += 1;
 }
-function changePerson() {
-  Object.assign(person, {name: '王五',age: 20})
+function changeC1() {
+  person.cars.c1.name = "林肯";
+}
+function changeC2() {
+  person.cars.c2.name = "林肯2";
+}
+function changeCar() {
+  person.cars = {
+    c1: {
+      name: "玛莎拉蒂1",
+      price: 100000,
+    },
+    c2: {
+      name: "玛莎拉蒂2",
+      price: 100000,
+    },
+  };
 }
 
-watch(person, (newValue, oldValue) => {
-  console.log('person值变化了', newValue, oldValue);
-})
+// 监视响应式对象中的某个基本类型的属性，需要写成函数式
+watch(() => person.name, (newValue, oldValue) => {
+  console.log("person.name值变化了", newValue, oldValue);
+});
+// 监视响应式对象中的某个对象类型的属性，最好写成函数式  但对象中的属性 需要开启deep:true
+// watch(person.cars, (newValue, oldValue) => {
+//   console.log("person.cars值变化了", newValue, oldValue);
+// });
+watch(() => person.cars, (newValue, oldValue) => {
+  console.log("person.cars值变化了", newValue, oldValue);
+},{deep: true});
 
+watch([() => person.name,() => person.cars.c1.name], (newValue, oldValue) => {
+  console.log("person.cars值变化了", newValue, oldValue);
+},{deep: true});
 </script>
 <style>
 .persion {
